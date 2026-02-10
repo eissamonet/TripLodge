4616a3157cd7c26b8d8f7597a43c4de638c3cb43
Eissa Purnell <eissamonet@gmail.com>
2026-02-10 13:16:29 -0600

    hero img added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import NavBar from './components/Navbar'
-import { useLocation } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './pages/Home';
 
 const App = () => {
 
@@ -10,6 +11,11 @@ const App = () => {
   return (
     <div>
       {!isOwnerPath && <NavBar />}
+      <div className='min-h-[70vh]'>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+        </Routes>
+      </div>
     </div>
   )
 }
