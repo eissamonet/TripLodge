315874a8287d9974b2a67ef52c49c2c59a64ae95
Eissa Purnell <eissamonet@gmail.com>
2026-02-10 12:54:40 -0600

    commit changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -55,7 +55,7 @@ const NavBar = () => {
 
                 {/* Mobile Menu Button */}
                 <div className="flex items-center gap-3 md:hidden">
-                    <img src={assets.menuIcon} alt="" className={`${isScrolled && 'invert'} h-4`} />
+                    <img onClick={()=> setIsMenuOpen(!isMenuOpen)} src={assets.menuIcon} alt="" className={`${isScrolled && 'invert'} h-4`} />
                 </div>
 
                 {/* Mobile Menu */}
