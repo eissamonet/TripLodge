a22d0ddbe319b892e04b3bb8b3b25f1f88370866
Eissa Purnell <eissamonet@gmail.com>
2026-02-12 08:25:58 -0600

    commit changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { assets } from "../assets/assets"
+import { useClerk, useUser, UserButton } from "@clerk/clerk-react";
 
 
 
@@ -15,6 +16,9 @@ const NavBar = () => {
 
     const [isScrolled, setIsScrolled] = React.useState(false);
     const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+    const {openSignIn} = useClerk()
+    const {user} = useUser()
 
     React.useEffect(() => {
         const handleScroll = () => {
@@ -48,7 +52,7 @@ const NavBar = () => {
                 {/* Desktop Right */}
                 <div className="hidden md:flex items-center gap-4">
                     <img src={assets.searchIcon} alt="search" className={`${isScrolled && 'invert'} h-7 transition-all duration-500`} />
-                    <button className={`px-8 py-2.5 rounded-full ml-4 transition-all duration-500 ${isScrolled ? "text-white bg-black" : "bg-white text-black"}`}>
+                    <button onClick={openSignIn} className="bg-black text-white px-8 py-2.5 rounded-full transition-all duration-500">
                         Login
                     </button>
                 </div>
@@ -74,7 +78,7 @@ const NavBar = () => {
                         Dashboard
                     </button>
 
-                    <button className="bg-black text-white px-8 py-2.5 rounded-full transition-all duration-500">
+                    <button onClick={openSignIn} className="bg-black text-white px-8 py-2.5 rounded-full transition-all duration-500">
                         Login
                     </button>
                 </div>
