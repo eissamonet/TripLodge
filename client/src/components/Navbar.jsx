786b9aebeff63a60917b705e3979c7ec956856d6
Eissa Purnell <eissamonet@gmail.com>
2026-02-13 08:52:45 -0600

    Login signup updated

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -101,7 +101,7 @@ const NavBar = () => {
                         Dashboard
                     </button>}
 
-                    {user && <button onClick={openSignIn} className="bg-black text-white px-8 py-2.5 rounded-full transition-all duration-500">
+                    {!user && <button onClick={openSignIn} className="bg-black text-white px-8 py-2.5 rounded-full transition-all duration-500">
                         Login
                     </button>}
                 </div>
