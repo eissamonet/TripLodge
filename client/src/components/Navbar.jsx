6e7b0db2b3e54f21b69dca47ed0fb7719092a6a2
Eissa Purnell <eissamonet@gmail.com>
2026-02-15 16:26:50 -0600

    commit changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -27,12 +27,20 @@ const NavBar = () => {
     const location = useLocation()
 
     useEffect(() => {
+
+        if(location.pathname !== '/'){
+            setIsScrolled(true);
+        }else{
+            setIsScrolled(false)
+        }
+        setIsScrolled(prev => location.pathname !== '/' ? true : prev);
+
         const handleScroll = () => {
             setIsScrolled(window.scrollY > 10);
         };
         window.addEventListener("scroll", handleScroll);
         return () => window.removeEventListener("scroll", handleScroll);
-    }, []);
+    }, [location.pathname]);
 
     return (
             <nav className={`fixed top-0 left-0 w-full flex items-center justify-between px-4 md:px-16 lg:px-24 xl:px-32 transition-all duration-500 z-50 ${isScrolled ? "bg-white/80 shadow-md text-gray-700 backdrop-blur-lg py-3 md:py-4" : "py-4 md:py-6"}`}>
