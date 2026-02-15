71e6abc7af6ce8f8e56ac8bd88735ad9f8903187
Eissa Purnell <eissamonet@gmail.com>
2026-02-15 17:11:11 -0600

    styling added

--- client/src/components/HotelCard.jsx
+++ client/src/components/HotelCard.jsx
@@ -6,10 +6,10 @@ const HotelCard = ({room, index}) => {
   return (
     <Link to={'/rooms/' + room._id} onClick={()=> scrollTo(0,0)} key={room._id}>
         <img src={room.images[0]} alt="" />
-        <p>Best Seller</p>
-        <div>
-            <div>
-                <p>{room.hotel.name}</p>
+        <p className='px-3 py-1 absolute top-3 left-3 text-xs bg-white text-gray-800 font-medium rounded-full'>Best Seller</p>
+        <div className='p-4 pt-5'>
+            <div className='flex items-center justify-between'>
+                <p className='font-playfair text-xl font-medium text-gray-800'>{room.hotel.name}</p>
                 <div className='flex items-center gap-1'>
                     <img src={assets.starIconFilled} alt='star-icon' /> 4.5
                 </div>
