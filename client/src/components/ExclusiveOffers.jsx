f978c45208cda5fcf87905977f71bd1be286b504
Eissa Purnell <eissamonet@gmail.com>
2026-02-19 09:58:55 -0600

    commit changes

--- client/src/components/ExclusiveOffers.jsx
+++ client/src/components/ExclusiveOffers.jsx
@@ -4,14 +4,18 @@ import { assets } from '../assets/assets'
 
 const ExclusiveOffers = () => {
   return (
-    <div>
-      <div>
+    <div className='flex flex-col items-center px-6 md:px-16 lg:px-24 xl:px-32 pt-20 pb-30'>
+      <div className='flex flex-col md:flex-row items-center justify-between gap-10 w-full'>
         <Title align='left' title='Exclusive Ofers' subTitle='Take advantage of pur limited-time offers and special packages to enhance your stat and
         create unforgettable memories.' />
         <button>
             View All Offers
             <img src={assets.arrowIcon} alt="arrow-icon" className='group-hover:translate-x-1 transition-all' />
         </button>
+      </div>
+
+      <div>
+
       </div>
     </div>
   )
