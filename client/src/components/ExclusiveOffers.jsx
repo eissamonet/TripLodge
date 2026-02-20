cf332a07bde369eed9dbc6995f92a1149daecfa4
Eissa Purnell <eissamonet@gmail.com>
2026-02-20 10:26:06 -0600

    commit changes

--- client/src/components/ExclusiveOffers.jsx
+++ client/src/components/ExclusiveOffers.jsx
@@ -17,8 +17,17 @@ const ExclusiveOffers = () => {
       <div>
         {exclusiveOffers.map((item) => (
             <div key={item._id} className='group relative flex flex-col items-start justify-between gap-1 pt-12 md:pt-18
-            rounded-xl text-white bg-no-repeat bg-cover bg-center'>
-
+            rounded-xl text-white bg-no-repeat bg-cover bg-center' style={{backgroundImage: `url(${item.image})`}}>
+               <p className='px-3 py-1 absolute top-4 left-4 text-xs bg-white text-gray-800 font-medium rounded-full'>{item.priceOff}% OFF</p>
+               <div>
+                 <p>{item.title}</p>
+                 <p>{item.description}</p>
+                 <p>Expires{item.expiryDate}</p>
+               </div>
+               <button>
+                View Offers
+                <img className='invert group-hover:translate-x-1 transition-all' src={assets.arrowIcon} alt='arrow-icon' />
+               </button>
             </div>
         ))}
       </div>
