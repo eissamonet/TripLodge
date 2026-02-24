22cd86891178c6a651d929413ec345dba9071fd5
Eissa Purnell <eissamonet@gmail.com>
2026-02-23 19:11:48 -0600

    allrooms page added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -45,10 +45,11 @@ const NavBar = () => {
     return (
             <nav className={`fixed top-0 left-0 w-full flex items-center justify-between px-4 md:px-16 lg:px-24 xl:px-32 transition-all duration-500 z-50 ${isScrolled ? "bg-white/80 shadow-md text-gray-700 backdrop-blur-lg py-3 md:py-4" : "py-4 md:py-6"}`}>
 
-                {/* Logo */}
-                <Link to='/' >
-                    <img src={assets.logo} alt="logo" className={`h-9 ${isScrolled && "invert opacity-80"}`} />
-                </Link>
+                <div className="flex items-center gap-1">
+                    <h1 className={`font-playfair text-4xl ${isScrolled ? "text-gray-800" : "text-white"}`}>
+                        TripLodge
+                    </h1>
+                </div>
 
                 {/* Desktop Nav */}
                 <div className="hidden md:flex items-center gap-4 lg:gap-8">
