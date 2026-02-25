a2b503c7a2aed044fe8134487c84753b652d3184
Eissa Purnell <eissamonet@gmail.com>
2026-02-25 10:12:53 -0600

    start rating info added

--- client/src/pages/AllRooms.jsx
+++ client/src/pages/AllRooms.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { roomsDummyData } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
+import StarRating from '../components/StarRating';
 
 const AllRooms = () => {
+  const navigate = useNavigate();
   return (
     <div className='flex flex-col-reverse lg:flex-row items-start justify-between pt-28 md:pt-35 px-4 md:px-16 lg:px-24
     xl:px-32'>
@@ -13,7 +16,16 @@ const AllRooms = () => {
 
         {roomsDummyData.map((room) => (
           <div>
-            <img src={room.images[0]} alt='hotel-img' title='View Room Details' className='max-h-65 md:w-1/2 rounded-xl shadow-lg object-cover cursor-pointer'/>
+            <img onClick={()=> navigate(`/rooms/${room._id}`)}
+            src={room.images[0]} alt='hotel-img' title='View Room Details'
+            className='max-h-65 md:w-1/2 rounded-xl shadow-lg object-cover cursor-pointer'/>
+            <div>
+              <p>{room.hotel.city}</p>
+              <p>{room.hotel.name}</p>
+              <div className='flex items-center'>
+                <StarRating />
+              </div>
+            </div>
           </div>
         ))}
 
