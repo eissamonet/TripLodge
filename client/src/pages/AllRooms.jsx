956f0bc80645899073cdb06094743f94a6008fec
Eissa Purnell <eissamonet@gmail.com>
2026-02-26 19:42:09 -0600

    room details page added

--- client/src/pages/AllRooms.jsx
+++ client/src/pages/AllRooms.jsx
@@ -113,7 +113,7 @@ const AllRooms = () => {
               <CheckBox key={index} label={`$ ${range}`} />
             ))}
           </div>
-           <div className='px-5 pt-5'>
+           <div className='px-5 pt-5 pb-7'>
             <p className='font-medium text-gray-800 pb-2'>Sort By</p>
             {sortOptions.map((option, index) => (
               <RadioButton key={index} label={option} />
