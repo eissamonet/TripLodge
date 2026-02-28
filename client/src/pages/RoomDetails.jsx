53d82214d4f29199843ecc3b38b5752c3dd3e602
Eissa Purnell <eissamonet@gmail.com>
2026-02-28 11:30:04 -0600

    additional images added

--- client/src/pages/RoomDetails.jsx
+++ client/src/pages/RoomDetails.jsx
@@ -41,6 +41,13 @@ const RoomDetails = () => {
          <div className='lg:w-1/2 w-full'>
             <img src={mainImage} alt='room-image' className='w-full rounded-xl shadow-lg object-cover' />
          </div>
+         <div className='grid grid-cols-2 gap-4 lg:w-1/2 w-full'>
+            {room?.images.length > 1 && room.images.map((image, index) => (
+                <img onClick={() => setMainImage(image)}
+                key={index} src={image} alt='Room-Image'
+                className={`w-full rounded-xl shadow-md object-cover cursor-pointer ${mainImage === image && 'outline-3 outline-orange-500'}`} />
+            ))}
+         </div>
        </div>
     </div>
   )
