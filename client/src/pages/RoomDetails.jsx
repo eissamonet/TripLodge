66d6e3f318750fb17ff246d5e14a75121611e9a0
Eissa Purnell <eissamonet@gmail.com>
2026-02-28 12:23:06 -0600

    commit changes

--- client/src/pages/RoomDetails.jsx
+++ client/src/pages/RoomDetails.jsx
@@ -67,6 +67,16 @@ const RoomDetails = () => {
         <p className='text-2-xl font-medium'>${room.pricePerNight}/night</p>
       </div>
 
+       {/* checkin checkout form */}
+       <form className='flex flex-col md:flex-row items-start md:items-center justify-between bg-white shadow-[0px_0px_20px_rgba(0,0,0,0.15)] p-6
+       rounded-xl mx-auto mt-16 max-w-6xl'>
+           <div>
+           </div>
+           <button type='submit' className='bg-primary hover:bg-primary-dull active:scale-95 transition-all text-white rounded-md max-md:w-full max-md:mt-6 md:px-25 py-3 md:py-4 text-base cursor-pointer'>
+            Book Now
+           </button>
+       </form>
+
 
     </div>
   )
