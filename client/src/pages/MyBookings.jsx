f77842e79a5e0e83bc9fd4982d82055c5cf5cb91
Eissa Purnell <eissamonet@gmail.com>
2026-03-02 09:05:20 -0600

    my bookings page added

---
+++ client/src/pages/MyBookings.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Title from '../components/Title'
+
+const MyBookings = () => {
+  return (
+    <div>
+      <Title title="My Bookings" subTitle="Easily manage your past, current and upcoming reservations in one place.
+      Plan your trips seamlessly with just a few clicks!" align="left" />
+    </div>
+  )
+}
+
+export default MyBookings
