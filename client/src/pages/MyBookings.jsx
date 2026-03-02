ba1e65c1c423636f6e93ff7b531efe421b9747b6
Eissa Purnell <eissamonet@gmail.com>
2026-03-02 09:58:43 -0600

    commit changes

--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -1,24 +1,38 @@
-import React from 'react'
-import Title from '../components/Title'
+import React, { useState } from "react";
+import Title from "../components/Title";
+import { userBookingsDummyData } from "../assets/assets";
 
 const MyBookings = () => {
+  const [bookings, setBookings] = useState(userBookingsDummyData);
   return (
-    <div className='py-28 md:pb-35 md:pt-32 px-4 md:px-16 lg:px-24 xl:px-32'>
+    <div className="py-28 md:pb-35 md:pt-32 px-4 md:px-16 lg:px-24 xl:px-32">
+      <Title
+        title="My Bookings"
+        subTitle="Easily manage your past, current and upcoming reservations in one place.
+      Plan your trips seamlessly with just a few clicks!"
+        align="left"
+      />
 
-      <Title title="My Bookings" subTitle="Easily manage your past, current and upcoming reservations in one place.
-      Plan your trips seamlessly with just a few clicks!" align="left" />
-
-      <div className='max-w-6xl mt-8 w-full text-gray-800'>
-
-        <div className='hidden md:grid md:grid-cols-[3fr_2fr_1fr] w-full border-b border-gray-300 font-medium text-base py-3'>
-           <div className='w-1/3'>Hotels</div>
-           <div className='w-1/3'>Date & Timings</div>
-           <div className='w-1/3'>Payment</div>
+      <div className="max-w-6xl mt-8 w-full text-gray-800">
+        <div className="hidden md:grid md:grid-cols-[3fr_2fr_1fr] w-full border-b border-gray-300 font-medium text-base py-3">
+          <div className="w-1/3">Hotels</div>
+          <div className="w-1/3">Date & Timings</div>
+          <div className="w-1/3">Payment</div>
         </div>
 
+        {bookings.map((booking) => (
+          <div key={booking._id}>
+            {/* hotel details */}
+            <div></div>
+            {/* date and time */}
+            <div></div>
+            {/* payment details */}
+            <div></div>
+          </div>
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default MyBookings
+export default MyBookings;
