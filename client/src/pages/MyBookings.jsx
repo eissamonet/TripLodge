899ece5a1642ad910c87964e6f5ff59df86ba5d3
Eissa Purnell <eissamonet@gmail.com>
2026-03-02 09:24:56 -0600

    commit changes

--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -4,8 +4,10 @@ import Title from '../components/Title'
 const MyBookings = () => {
   return (
     <div className='py-28 md:pb-35 md:pt-32 px-4 md:px-16 lg:px-24 xl:px-32'>
+
       <Title title="My Bookings" subTitle="Easily manage your past, current and upcoming reservations in one place.
       Plan your trips seamlessly with just a few clicks!" align="left" />
+
     </div>
   )
 }
