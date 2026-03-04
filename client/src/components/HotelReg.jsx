76f0cba2d8585f825433c21e557a8e4db69b6bae
Eissa Purnell <eissamonet@gmail.com>
2026-03-04 09:45:32 -0600

    hotel registration comp added

---
+++ client/src/components/HotelReg.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HotelReg = () => {
+  return (
+    <div className='fixed top-0 bottom-0 left-0 z-100 flex items-center justify-center bg-black/70'>
+
+    </div>
+  )
+}
+
+export default HotelReg
