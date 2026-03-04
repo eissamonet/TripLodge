5dee07039824e60dea4901ead54acbd20d05fcf7
Eissa Purnell <eissamonet@gmail.com>
2026-03-04 17:50:20 -0600

    city options added

--- client/src/components/HotelReg.jsx
+++ client/src/components/HotelReg.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { assets } from '../assets/assets'
+import { assets, cities } from '../assets/assets'
 
 const HotelReg = () => {
   return (
@@ -33,6 +33,10 @@ const HotelReg = () => {
           <div className='w-full mt-4 max-w-60 mr-auto '>
             <label htmlFor='city' className='font-medium text-gray-500'>City</label>
             <select id='city' className='border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light cursor-pointer' required>
+               <option value="">Select City</option>
+               {cities.map((city) => (
+                <option key={city} value={city}>{city}</option>
+               ))}
             </select>
           </div>
         </div>
