426808ef2f3b320b33c5d0d0802e887d3cd8f2ac
Eissa Purnell <eissamonet@gmail.com>
2026-03-04 17:08:06 -0600

    commit changes

--- client/src/components/HotelReg.jsx
+++ client/src/components/HotelReg.jsx
@@ -3,14 +3,20 @@ import { assets } from '../assets/assets'
 
 const HotelReg = () => {
   return (
-    <div className='fixed top-0 bottom-0 left-0 z-100 flex items-center justify-center bg-black/70'>
-      <form className='flex bg-white rounded -xl max-w-4xl max-:mdmx-2'>
+    <div className='fixed top-0 bottom-0 left-0 right-0 z-100 flex items-center justify-center bg-black/70'>
+      <form className='flex bg-white rounded-xl max-w-4xl max-md:mx-2'>
         <img src={assets.regImage} alt='red-image' className='w-1/2 rounded-xl hidden md:block' />
 
         <div className='relative flex flex-col items-center md:w-1/2 p-8 md:p-10'>
           <img src={assets.closeIcon} alt="close-icon" className='absolute top-4 right-4 h-4 w-4 cursor-pointer' />
-          <p className='text-2xl font-semi-bold mt-6'>Register Your Hotel </p>
+          <p className='text-2xl font-semi-bold mt-6'>Register Your Hotel</p>
+
+          {/* hotel name */}
+          <div className='w-full mt-4'>
+
+          </div>
         </div>
+
       </form>
     </div>
   )
