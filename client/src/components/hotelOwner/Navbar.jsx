d99d3284f85014c888ed4d2c80b017bc1b8c8d90
Eissa Purnell <eissamonet@gmail.com>
2026-03-05 12:02:12 -0600

    commit changes

---
+++ client/src/components/hotelOwner/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex items-center justify-between px-4 md:px-8 border-b border-gray-300 py-3 bg-white transition-all duration-300'>
+
+    </div>
+  )
+}
+
+export default Navbar
