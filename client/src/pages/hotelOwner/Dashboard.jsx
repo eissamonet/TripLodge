f87a22dd7b4c4f051da857659ae148b1b5dc003a
Eissa Purnell <eissamonet@gmail.com>
2026-03-05 14:36:42 -0600

    commit changes

--- client/src/pages/hotelOwner/Dashboard.jsx
+++ client/src/pages/hotelOwner/Dashboard.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
-import Title from '../../components/Title'
+import React from "react";
+import Title from "../../components/Title";
+import { assets } from "../../assets/assets";
 
 const Dashboard = () => {
   return (
     <div>
-       <Title align='left' font='outfit' title='Dashboard' subTitle='Monitor your room listings, track bookings and analyze revenue all in one place.
-       Stay updated with real-time insights to ensure smooth operations and maximize profits.' />
-    </div>
-  )
-}
+      <Title
+        align="left"
+        font="outfit"
+        title="Dashboard"
+        subTitle="Monitor your room listings, track bookings and analyze revenue all in one place.
+       Stay updated with real-time insights to ensure smooth operations and maximize profits."
+      />
 
-export default Dashboard
+      <div className="flex gap-4 my-8">
+        {/* total bookings */}
+        <div className="bg-primary/3 border border-primary/10 rounded flex p-4 pr-8">
+          <img src={assets.totalBookingIcon} alt="" className="max-sm:hidden h-10" />
+        </div>
+        {/* total revenue */}
+        <div>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
