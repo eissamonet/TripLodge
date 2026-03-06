deca4e89744b6f3e5359231db63ebdb650bf2e52
Eissa Purnell <eissamonet@gmail.com>
2026-03-05 18:02:54 -0600

    user dashboard added

--- client/src/pages/hotelOwner/Dashboard.jsx
+++ client/src/pages/hotelOwner/Dashboard.jsx
@@ -19,16 +19,35 @@ const Dashboard = () => {
         {/* total bookings */}
         <div className="bg-primary/3 border border-primary/10 rounded flex p-4 pr-8">
           <img src={assets.totalBookingIcon} alt="" className="max-sm:hidden h-10" />
-        </div>
-        <div className="flex flex-col sm:ml-4 font-medium">
-          <p className="text-blue-500 text-lg">Total Bookings</p>
-          <p className="text-neutral-400 text-base">{dashboardData.totalBookings}</p>
+          <div className="flex flex-col sm:ml-4 font-medium">
+            <p className="text-blue-500 text-lg">Total Bookings</p>
+            <p className="text-neutral-400 text-base">{dashboardData.totalBookings}</p>
+          </div>
         </div>
         {/* total revenue */}
-        <div>
-
+        <div className="bg-primary/3 border border-primary/10 rounded flex p-4 pr-8">
+          <img src={assets.totalRevenueIcon} alt="" className="max-sm:hidden h-10" />
+          <div className="flex flex-col sm:ml-4 font-medium">
+            <p className="text-blue-500 text-lg">Total Revenue</p>
+            <p className="text-neutral-400 text-base">$ {dashboardData.totalRevenue}</p>
+          </div>
         </div>
       </div>
+
+      {/* recent bookings */}
+      <h2 className="text-xl text-blue-950/70 font-medium mb-5">Recent Bookings</h2>
+
+        <div className="w-full max-w-3xl text-left border border-gray-300 rounded-lg max-h-80 overflow-y-scroll">
+
+          <table className="w-full">
+            <thead className="bg-gray-50">
+               <tr>
+                <th>User Name</th>
+               </tr>
+            </thead>
+          </table>
+
+        </div>
     </div>
   );
 };
