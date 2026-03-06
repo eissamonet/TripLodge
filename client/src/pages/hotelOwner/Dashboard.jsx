c1463eafb882b44ee9a249f2fb916c2b18a45a11
Eissa Purnell <eissamonet@gmail.com>
2026-03-05 18:33:28 -0600

    commit changes

--- client/src/pages/hotelOwner/Dashboard.jsx
+++ client/src/pages/hotelOwner/Dashboard.jsx
@@ -51,16 +51,32 @@ const Dashboard = () => {
       </h2>
 
       <div className="w-full max-w-3xl text-left border border-gray-300 rounded-lg max-h-80 overflow-y-scroll">
-        <table className="w-full">
-          <thead className="bg-gray-50">
-            <tr>
-              <th className="py-3 px-4 text-gray-800 font-medium">User Name</th>
-              <th className="py-3 px-4 text-gray-800 font-medium max-sm:hidden">Room Name</th>
-              <th className="py-3 px-4 text-gray-800 font-medium text-center">Total Amount</th>
-              <th className="py-3 px-4 text-gray-800 font-medium text-center">Payment Status</th>
-            </tr>
-          </thead>
-        </table>
+
+      <table className="w-full">
+      <thead className="bg-gray-50">
+        <tr>
+          <th className="py-3 px-4 text-gray-800 font-medium">User Name</th>
+          <th className="py-3 px-4 text-gray-800 font-medium max-sm:hidden">Room Name</th>
+          <th className="py-3 px-4 text-gray-800 font-medium text-center">Total Amount</th>
+          <th className="py-3 px-4 text-gray-800 font-medium text-center">Payment Status</th>
+        </tr>
+      </thead>
+
+    <tbody className="text-sm">
+      {dashboardData.bookings.map((item, index) =>(
+        <tr key={index}>
+          <td className="py-3 px-4 text-gray-700 border-t border-gray-300">
+            {item.user.username}
+          </td>
+
+          <td className="py-3 px-4 text-gray-700 border-t border-gray-300 max-sm:hidden">
+            {item.room.roomType}
+          </td>
+        </tr>
+    ))}
+    </tbody>
+
+      </table>
       </div>
     </div>
   );
