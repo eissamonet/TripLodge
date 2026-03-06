ec8d93466083873593d697ac0a3d9e53d57ec9bb
Eissa Purnell <eissamonet@gmail.com>
2026-03-06 08:46:41 -0600

    commit changes

--- client/src/pages/hotelOwner/AddRoom.jsx
+++ client/src/pages/hotelOwner/AddRoom.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const AddRoom = () => {
+
+  const [images, setImages] = useState({
+    1: null,
+    2: null,
+    3: null,
+    4: null,
+  })
+  cont [inputs, setInputs] = useState({
+    roomType: '',
+    pricePerNight: 0,
+    amenities: {
+      'WiFi': false,
+      'Free Breakfast': false,
+      'Air Conditioning': false,
+      'Pool Access': false,
+      'Mini Bar': false,
+      'Room Service': false,
+    }
+  })
+
   return (
     <div>
 
