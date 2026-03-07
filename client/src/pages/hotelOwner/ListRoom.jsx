bbfcd0518864aa770b11394507e6142ef7cbcd79
Eissa Purnell <eissamonet@gmail.com>
2026-03-07 10:00:32 -0600

    commit changes

--- client/src/pages/hotelOwner/ListRoom.jsx
+++ client/src/pages/hotelOwner/ListRoom.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
-import Title from '../../components/Title'
 import { roomsDummyData } from '../../assets/assets'
+import Title from '../../components/Title'
 
 const ListRoom = () => {
 
@@ -8,8 +8,8 @@ const ListRoom = () => {
 
   return (
     <div>
-      <Title align='left' font='outfit' titel='Room listings' subTitle='View, edit, or manage all listed rooms. Keep the information up-to-date to provide the best
-      experience for users.' />
+      <Title align='left' font='outfit' title='Room Listings' subTitle='View, edit, or manage all listed rooms. Keep the information up-to-date to provide the
+      best experience for users.' />
     </div>
   )
 }
