fd732a8812ccfe4ae619ff5c659a4484f450a08a
Eissa Purnell <eissamonet@gmail.com>
2026-03-07 10:18:29 -0600

    commit changes

--- client/src/pages/hotelOwner/ListRoom.jsx
+++ client/src/pages/hotelOwner/ListRoom.jsx
@@ -1,18 +1,53 @@
-import React, { useState } from 'react'
-import { roomsDummyData } from '../../assets/assets'
-import Title from '../../components/Title'
+import React, { useState } from "react";
+import { roomsDummyData } from "../../assets/assets";
+import Title from "../../components/Title";
 
 const ListRoom = () => {
-
-  const [rooms, setRooms] = useState(roomsDummyData)
+  const [rooms, setRooms] = useState(roomsDummyData);
 
   return (
     <div>
-      <Title align='left' font='outfit' title='Room Listings' subTitle='View, edit, or manage all listed rooms. Keep the information up-to-date to provide the
-      best experience for users.' />
-      <p className='text-gray-500 mt-8'>All Rooms</p>
-    </div>
-  )
-}
+      <Title
+        align="left"
+        font="outfit"
+        title="Room Listings"
+        subTitle="View, edit, or manage all listed rooms. Keep the information up-to-date to provide the
+      best experience for users."
+      />
+      <p className="text-gray-500 mt-8">All Rooms</p>
 
-export default ListRoom
+      <div className="w-full max-w-3xl text-left border border-gray-300 rounded-lg max-h-80 overflow-y-scroll mt-3">
+        <table className="w-full">
+          <thead className="bg-gray-50">
+            <tr>
+              <th className="py-3 px-4 text-gray-800 font-medium"> Name</th>
+              <th className="py-3 px-4 text-gray-800 font-medium max-sm:hidden">
+                Facility
+              </th>
+              <th className="py-3 px-4 text-gray-800 font-medium">
+                Price /night
+              </th>
+              <th className="py-3 px-4 text-gray-800 font-medium text-center">
+                Actions
+              </th>
+            </tr>
+          </thead>
+          <tbody className="text-sm">
+            {
+              rooms.map((item, index)=>(
+                <tr key={index}>
+                  <td className="py-3 px-4 text-gray-700 border-t border-gray-300">
+                    {item.roomType}
+                  </td>
+
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default ListRoom;
