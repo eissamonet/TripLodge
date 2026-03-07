5e93762636a25f0d4eff5471564b37e3677c332e
Eissa Purnell <eissamonet@gmail.com>
2026-03-07 13:59:33 -0600

    commit changes

--- client/src/pages/hotelOwner/ListRoom.jsx
+++ client/src/pages/hotelOwner/ListRoom.jsx
@@ -39,6 +39,9 @@ const ListRoom = () => {
                   <td className="py-3 px-4 text-gray-700 border-t border-gray-300">
                     {item.roomType}
                   </td>
+                  <td className="py-3 px-4 text-gray-700 border-t border-gray-300 max-sm:hidden">
+                    {item.amenities.join(', ')}
+                  </td>
 
                 </tr>
               ))
