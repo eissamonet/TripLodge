91676c5e7db82756ddd3ad12a354589354aad964
Eissa Purnell <eissamonet@gmail.com>
2026-03-07 10:02:04 -0600

    commit changes

--- client/src/pages/hotelOwner/ListRoom.jsx
+++ client/src/pages/hotelOwner/ListRoom.jsx
@@ -10,6 +10,7 @@ const ListRoom = () => {
     <div>
       <Title align='left' font='outfit' title='Room Listings' subTitle='View, edit, or manage all listed rooms. Keep the information up-to-date to provide the
       best experience for users.' />
+      <p className='text-gray-500 mt-8'>All Rooms</p>
     </div>
   )
 }
