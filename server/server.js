5f5e83e3589dac4d3c60c341a5f8a364e32977ac
Eissa Purnell <eissamonet@gmail.com>
2026-03-08 18:22:48 -0500

    commit changes

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 import cors from "cors";
 import connectDB from "./configs/db.js";
 import { clerkMiddleware } from '@clerk/express'
+import clerkWebhooks from "./controllers/clerkWebhooks.js";
 
 connectDB()
 
@@ -13,6 +14,9 @@ app.use(cors()) // enable cross-origin resource sharing
 // middleware
 app.use(express.json())
 app.use(clerkMiddleware())
+
+// api to listen to clerk webhooks
+app.use('/api/clerk', clerkWebhooks)
 
 app.get('/', (req, res)=> res.send("API is working"))
 
