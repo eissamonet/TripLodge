36deb34304349f6f4e98d240e45caeb8a1e0c71c
Eissa Purnell <eissamonet@gmail.com>
2026-03-08 15:13:56 -0500

    commit changes

--- server/controllers/clerkWebhooks.js
+++ server/controllers/clerkWebhooks.js
@@ -10,7 +10,21 @@ const clerkWebhooks = async (req, res)=>{
           "svix-id": req.headers['svix-id'],
           "svix-timestamp": req.headers['svix-timestamp'],
           "svix-signature": req.headers['svix-signature'],
+        };
+
+        // verifying headers
+        await whook.verify(JSON.stringify(req.body), headers)
+
+        // verifying headers
+        const {data, type} = req.body
+
+        const userDate = {
+            _id: data.id,
+            email: data.email_addresses[0].email_address,
+            username: data.first_name + '' + data.last_name,
+            image: data.image_url,
         }
+
     } catch (error) {
 
     }
