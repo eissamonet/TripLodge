1d70c7b000321766dbd847dc0ff2a8a1ce3055f7
Eissa Purnell <eissamonet@gmail.com>
2026-03-08 14:34:19 -0500

    commit changes

--- server/server.js
+++ server/server.js
@@ -2,12 +2,17 @@ import express from "express"
 import "dotenv/config";
 import cors from "cors";
 import connectDB from "./configs/db.js";
+import { clerkMiddleware } from '@clerk/express'
 
 connectDB()
 
 
 const app = express()
 app.use(cors()) // enable cross-origin resource sharing
+
+// middleware
+app.use(express.json())
+app.use(clerkMiddleware())
 
 app.get('/', (req, res)=> res.send("API is working"))
 
