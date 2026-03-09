7a06cc0c4f5c71d1cf8185bc9c3b62df6627a066
Eissa Purnell <eissamonet@gmail.com>
2026-03-08 20:37:47 -0500

    commit changes

--- server/configs/db.js
+++ server/configs/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async ()=>{
     try {
         mongoose.connection.on('connected', ()=> console.log("Database Connected"));
-        await mongoose.connect(`${process.env.MONGODB_URI}/trip-lodge`)
+        await mongoose.connect(`${process.env.MONGODB_URI}/triplodge`)
     } catch (error) {
         console.log(error.message);
     }
---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,17 @@
+import User from '../models/User.js';
+
+// middleware to check if user is authenticated
+export const protext = async (req, res, next) => {
+    try {
+        const userId = req.auth?.userId || req.auth;
+        if (!userId) {
+            return res.json({ success: false, message: 'not authenticated' });
+        } else {
+            const user = await User.findById(userId);
+            req.user = user;
+            next();
+        }
+    } catch (err) {
+        next(err);
+    }
+}
