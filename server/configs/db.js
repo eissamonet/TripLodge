772c32ebbc4b699cab1d412e633378b5c1832fa2
Eissa Purnell <eissamonet@gmail.com>
2026-03-08 21:05:49 -0500

    commit changes

--- server/configs/db.js
+++ server/configs/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async ()=>{
     try {
         mongoose.connection.on('connected', ()=> console.log("Database Connected"));
-        await mongoose.connect(`${process.env.MONGODB_URI}/triplodge`)
+        await mongoose.connect(`${process.env.MONGODB_URI}/trip-lodge`)
     } catch (error) {
         console.log(error.message);
     }
---
+++ server/routes/userRoutes.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import { protect } from '../middleware/authMiddleware.js';
+import { getUserData } from '../controllers/userController.js';
+
+const userRouter = express.Router();
+
+userRouter.get('/', protect, getUserData);
+
+export default userRouter;
