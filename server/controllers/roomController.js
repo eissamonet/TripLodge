b6a31da75659f9c33ac4a2fca160fb24d9dfcc19
Eissa Purnell <eissamonet@gmail.com>
2026-03-12 09:30:44 -0500

    commit changes

--- server/controllers/roomController.js
+++ server/controllers/roomController.js
@@ -1,17 +1,22 @@
 
 // api to create a new room for a hotel
-export const createRoom =async (req, res)=>{
+export const createRoom = async (req, res)=>{
 
 }
 
 
 // api to get all rooms
-export const getRooms =async (req, res)=>{
+export const getRooms = async (req, res)=>{
 
 }
 
 
 // api to get all rooms for a specific hotel
-export const getOwnerRooms =async (req, res)=>{
+export const getOwnerRooms = async (req, res)=>{
+
+}
+
+// api to toggle availability of a room
+export const toggleRoomAvailability = async (req, res)=>{
 
 }
