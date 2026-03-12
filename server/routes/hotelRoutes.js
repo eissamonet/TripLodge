bad34be5b0264a14fab2470393a9cd5b603acd84
Eissa Purnell <eissamonet@gmail.com>
2026-03-12 09:13:07 -0500

    hotel route added

--- server/routes/hotelRoutes.js
+++ server/routes/hotelRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { protect } from "../middleware/authMiddleware.js";
+import { registerHotel } from "../controllers/hotelController.js";
 
 const hotelRouter = express.Router();
 
-hotelRouter.post('/', protect)
+hotelRouter.post('/', protect, registerHotel);
