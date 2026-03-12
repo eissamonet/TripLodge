8d0ff36a03669c279d340add4913d808cacae5b8
Eissa Purnell <eissamonet@gmail.com>
2026-03-12 09:14:36 -0500

    commit changes

--- server/routes/hotelRoutes.js
+++ server/routes/hotelRoutes.js
@@ -5,3 +5,5 @@ import { registerHotel } from "../controllers/hotelController.js";
 const hotelRouter = express.Router();
 
 hotelRouter.post('/', protect, registerHotel);
+
+export default hotelRouter;
