c746130f13579f1c5bd6a968023c1dc14e1d856f
Eissa Purnell <eissamonet@gmail.com>
2026-03-12 09:29:23 -0500

    room controller added

---
+++ server/controllers/roomController.js
@@ -0,0 +1,17 @@
+
+// api to create a new room for a hotel
+export const createRoom =async (req, res)=>{
+
+}
+
+
+// api to get all rooms
+export const getRooms =async (req, res)=>{
+
+}
+
+
+// api to get all rooms for a specific hotel
+export const getOwnerRooms =async (req, res)=>{
+
+}
