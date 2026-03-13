6956823fea968f4d27aa302897330646e4087692
Eissa Purnell <eissamonet@gmail.com>
2026-03-13 08:49:52 -0500

    commit changes

--- server/controllers/roomController.js
+++ server/controllers/roomController.js
@@ -1,7 +1,15 @@
+import Hotel from "../models/Hotel";
 
 // api to create a new room for a hotel
 export const createRoom = async (req, res)=>{
+    try {
+        const {roomType, pricePerNight, amenities} = req.body;
+        const hotel = await Hotel.findOne({owner: req.auth.userId})
 
+        if(!hotel) return res.json({ success: false, message: "No Hotel Found"});
+    } catch (error) {
+
+    }
 }
 
 
