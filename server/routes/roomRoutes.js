05f28f36fb9d4e011ac8796affe45b590bfbe65b
Eissa Purnell <eissamonet@gmail.com>
2026-03-15 17:32:33 -0500

    room routes updated

--- server/routes/roomRoutes.js
+++ server/routes/roomRoutes.js
@@ -8,7 +8,7 @@ const roomRouter = express.Router();
 roomRouter.post('/', upload.array('images', 4), protect, createRoom)
 roomRouter.get('/', getRooms)
 roomRouter.get('/owner', protect, getOwnerRooms)
-roomRouter.get('/toggle-availability', protect, toggleRoomAvailability)
+roomRouter.post('/toggle-availability', protect, toggleRoomAvailability)
 
 
 
