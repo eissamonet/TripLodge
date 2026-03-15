97d28b68b48c7b6d10c659dde2012f2a44f2b198
Eissa Purnell <eissamonet@gmail.com>
2026-03-15 16:36:05 -0500

    commit changes

--- server/routes/roomRoutes.js
+++ server/routes/roomRoutes.js
@@ -6,3 +6,5 @@ import { createRoom } from "../controllers/roomController.js";
 const roomRouter = express.Router();
 
 roomRouter.post('/', upload.array('images', 4), protect, createRoom)
+
+export default roomRouter;
