51c6deafd8a59b8a809df49ed352a9b235baf24b
Eissa Purnell <eissamonet@gmail.com>
2026-03-15 17:15:46 -0500

    commit changes

--- server/controllers/roomController.js
+++ server/controllers/roomController.js
@@ -55,6 +55,14 @@ export const getRooms = async (req, res)=>{
 
 // api to get all rooms for a specific hotel
 export const getOwnerRooms = async (req, res)=>{
+    try {
+        const hotelData = await Hotel({owner: req.auth.userId})
+        const rooms = await Room.find({hotel: hotelData._id.toString()}).populate
+        ('hotel');
+        res.json({success: true, rooms});
+    } catch (error) {
+        res.json({success: false, message: error.message});
+    }
 
 }
 
