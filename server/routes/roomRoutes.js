80d2e8d0ff9f96ac065c1980501c074e282730d1
Eissa Purnell <eissamonet@gmail.com>
2026-03-15 17:29:38 -0500

    room routes updated

--- server/routes/roomRoutes.js
+++ server/routes/roomRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import upload from "../middleware/uploadMiddleware.js";
 import { protect } from "../middleware/authMiddleware.js";
-import { createRoom } from "../controllers/roomController.js";
+import { createRoom, getRooms } from "../controllers/roomController.js";
 
 const roomRouter = express.Router();
 
 roomRouter.post('/', upload.array('images', 4), protect, createRoom)
+roomRouter.get('/', getRooms)
+
 
 export default roomRouter;
