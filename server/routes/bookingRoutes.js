a91e4dd268ea6146373946cdabe6007634c1d756
Eissa Purnell <eissamonet@gmail.com>
2026-03-17 09:56:41 -0500

    commit changes

--- server/routes/bookingRoutes.js
+++ server/routes/bookingRoutes.js
@@ -1,6 +1,10 @@
 import express from 'express';
-import { checkAvailabilityAPI } from '../controllers/bookingController.js';
+import { checkAvailabilityAPI, createBooking, getHotelBookings, getUserBookings } from '../controllers/bookingController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 const bookingRouter = express.Router();
 
 bookingRouter.post('/check-availability', checkAvailabilityAPI)
+bookingRouter.post('/book', protect, createBooking);
+bookingRouter.get('/user', protect, getUserBookings);
+bookingRouter.get('/hotel', protect, getHotelBookings);
