99a0b4776607f8e9fab2cf6eea73f212d2a1b44b
Eissa Purnell <eissamonet@gmail.com>
2026-03-17 20:29:29 -0500

    commit changes

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -2,6 +2,7 @@ import axios from  "axios";
 import { createContext, useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useUser, useAuth } from "@clerk/clerk-react"
+import { toast } from 'react-hot-toast'
 
 axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
 
@@ -31,7 +32,7 @@ export const AppProvider = ({ children })=>{
             },5000)
            }
         } catch (error) {
-
+            toast.error(error.message)
         }
     }
 
