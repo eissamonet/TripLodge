abefa03110a92ebc328e27cecf95d897d3277149
Eissa Purnell <eissamonet@gmail.com>
2026-03-17 19:52:52 -0500

    commit changes

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,4 +1,20 @@
 import axios from  "axios";
+import { createContext } from "react";
 
-axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
+
+const AppContext = createContext();
+
+export const AppProvider = ({ children })=>{
+
+    const value ={
+
+    }
+
+    return (
+        <AppContext.Provider value={value}>
+            {children}
+        </AppContext.Provider>
+    )
+}
 
