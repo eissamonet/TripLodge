b1f7a91d1c8de3c4960fe4ce769a9a7e2fd363eb
Eissa Purnell <eissamonet@gmail.com>
2026-03-20 09:25:12 -0500

    commit changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
 
 
   const isOwnerPath = useLocation().pathname.includes('owner');
-  const {showHotelReg} = useAppContext
+  const {showHotelReg} = useAppContext();
 
   return (
     <div>
