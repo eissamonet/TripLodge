da0a24a7b6dd1a02d7832e49070dfc1dbd2f3091
Eissa Purnell <eissamonet@gmail.com>
2026-03-29 20:30:00 -0500

    commit chnages

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,17 +1,13 @@
 import User from '../models/User.js';
 
-// middleware to check if user is authenticated
+// middleware to check if User is authenticated
 export const protect = async (req, res, next) => {
-    try {
-        const userId = req.auth?.userId || req.auth;
-        if (!userId) {
-            return res.json({ success: false, message: 'not authenticated' });
-        } else {
-            const user = await User.findOne({ clerkId: userId });
-            req.user = user;
-            next();
-        }
-    } catch (err) {
-        next(err);
+    const {userId} = req.auth;
+    if(!userId){
+        res.json({success: false, message: "Not Authorized"})
+    }else{
+        const user = await User.findById(userId);
+        req.user = user;
+        next();
     }
 }
