77c39e4ce05a2ede742d92220ef5c3343c88c9c8
Eissa Purnell <eissamonet@gmail.com>
2026-04-17 09:17:12 -0500

    fetchrooms updated

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -24,6 +24,22 @@ export const AppProvider = ({ children })=>{
     const [isOwner, setIsOwner] = useState(false)
     const [showHotelReg, setShowHotelReg] = useState(false)
     const [searchedCities, setSearchedCities] = useState([])
+    const [rooms, setRooms] = useState([])
+
+    const fetchRooms = async ()=>{
+       try {
+        const { data } = await axiosInstance.get('/api/rooms')
+        if (data.success){
+            setRooms(data.rooms)
+        }else{
+            toast.error(data.message)
+        }
+       } catch (error) {
+        toast.error(error.message)
+       }
+    }
+
+
 
     const fetchUser = useCallback(async () => {
     try {
