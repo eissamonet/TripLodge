27ac0173ff5e63e2883a704256f5c8ceb447c731
Eissa Purnell <eissamonet@gmail.com>
2026-04-16 19:59:47 -0500

    dashboard data updated

--- client/src/pages/hotelOwner/Dashboard.jsx
+++ client/src/pages/hotelOwner/Dashboard.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import Title from "../../components/Title";
 import { assets } from "../../assets/assets";
+import { useAppContext } from "../../context/AppContext";
 
 const Dashboard = () => {
 
-  const { currency, user, getToken, toast, axiosInstance } = useAppContext();
+  const { currency, user, getToken, toast, axiosInstance } = useAppContext()
 
   const [dashboardData, setDashboardData] = useState({
     bookings: [],
@@ -66,7 +67,7 @@ const Dashboard = () => {
           <div className="flex flex-col sm:ml-4 font-medium">
             <p className="text-blue-500 text-lg">Total Revenue</p>
             <p className="text-neutral-400 text-base">
-              $ {dashboardData.totalRevenue}
+              {currency} {dashboardData.totalRevenue}
             </p>
           </div>
         </div>
@@ -101,7 +102,7 @@ const Dashboard = () => {
           </td>
 
           <td className="py-3 px-4 text-gray-700 border-t border-gray-300 text-center">
-            $ {item.totalPrice}
+            {currency} {item.totalPrice}
           </td>
 
           <td className="py-3 px-4 border-t border-gray-300 flex">
