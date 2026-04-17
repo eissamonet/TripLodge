e574d0e7c5e898360d58bda7f4e6d6247d5646f8
Eissa Purnell <eissamonet@gmail.com>
2026-04-16 19:32:35 -0500

    toggle change updated

--- client/src/pages/hotelOwner/ListRoom.jsx
+++ client/src/pages/hotelOwner/ListRoom.jsx
@@ -80,7 +80,7 @@ const ListRoom = () => {
                   </td>
                   <td className="py-3 px-4 border-t border-gray-300 text-sm text-red-500 text-center">
                     <label className="relative inline-flex items-center cursor-pointer text-gray-900 gap-3">
-                        <input type="checkbox" className="sr-only peer" checked={item.isAvailable}/>
+                        <input onChange={()=> toggleAvailability(item._id)} type="checkbox" className="sr-only peer" checked={item.isAvailable}/>
                         <div className="w-12 h-7 bg-slate-300 rounded-full peer peer-checked:bg-blue-600 transition-colors duration-200"></div>
                         <span className="dot absolute left-1 top-1 w-5 h-5 bg-white rounded-full transition-transform duration-200 ease-in-out peer-checked:translate-x-5"></span>
                     </label>
