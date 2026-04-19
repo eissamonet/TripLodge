7d7f48eecb4cceb433556cd981f7639c96395b39
Eissa Purnell <eissamonet@gmail.com>
2026-04-19 18:37:41 -0500

    commit changes

--- client/src/components/RecommendedHotels.jsx
+++ client/src/components/RecommendedHotels.jsx
@@ -16,22 +16,18 @@ const RecommendedHotels = () => {
     filterHotels();
   },[rooms, searchedCities]);
 
-  return rooms.length > 0 && (
+  return recommended.length > 0 && (
     <div className='flex flex-col items-center px-6 md:px-16 lg:px-24 bg-slate-50 py-20'>
 
-      <Title title='Featured Destination' subTitle='Discover our handpicked selection of exceptional properties around the world,
+      <Title title='Recommended Hotels' subTitle='Discover our handpicked selection of exceptional properties around the world,
       offering unparralledled luxury and unforgettable experiences.' />
 
       <div className='flex flex-wrap items-center justify-center gap-6 mt-20'>
-        {rooms.slice(0,4).map((room, index)=>(
+        {recommended.slice(0,4).map((room, index)=>(
             <HotelCard key={room._id} room={room} index={index} />
         ))}
       </div>
 
-      <button onClick={()=>{navigate('/rooms'); scrollTo(0,0)}}
-      className='my-16 px-4 py-2 text-sm font-medium border border-gray-300 rounded bg-white hover:bg-gray-50 transition-all cursor-pointer'>
-        View All Destinations
-      </button>
     </div>
   )
 }
