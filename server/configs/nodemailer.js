e94af895609e4a7cca6c9b02913415049ad810be
Eissa Purnell <eissamonet@gmail.com>
2026-04-22 09:48:46 -0500

    nodemail updated

--- server/configs/nodemailer.js
+++ server/configs/nodemailer.js
@@ -4,8 +4,8 @@ const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
   port: 587,
   auth: {
-    user: "",
-    pass: "",
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
   },
 });
 
