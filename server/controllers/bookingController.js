955af4c9b0993cda48de91431796c5046e586365
Eissa Purnell <eissamonet@gmail.com>
2026-04-24 08:40:56 -0500

    updated req.auth function

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -9,14 +9,14 @@ const checkAvailability = async ({ checkInDate, checkOutDate, room })=>{
     try {
         const bookings = await Booking.find({
             room,
-            checkInDate: {$lte: checkOutDate},
-            checkOutDate: {$lte: checkInDate},
+            checkInDate: {$lt: checkOutDate},
+            checkOutDate: {$gt: checkInDate},
         });
         const isAvailable = bookings.length === 0;
         return isAvailable;
     } catch (error) {
         console.error(error.message);
-
+        return false;
     }
 }
 
