93f120a38d3ec72219ae6a0360f26ff23158e704
Eissa Purnell <eissamonet@gmail.com>
2026-04-28 09:22:33 -0500

    commit changes

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { use } from 'react'
 import { useAppContext } from '../context/AppContext'
 import { useParams } from 'react-router-dom'
 
@@ -7,8 +7,17 @@ const Loader = () => {
     const { navigate } = useAppContext()
     const {nextUrl} = useParams()
 
+    use(() => {
+        if(nextUrl){
+            setTimeout(() => {
+                navigate(`/${nextUrl}`)
+            }, 8000)
+        }
+    },[nextUrl])
+
   return (
-    <div>
+    <div className='flex justify-center items-center h-screen'>
+        <div className='animate-spin rounded-full h-24 w-24 border-4 border-gray-300 border-t-primary'></div>
 
     </div>
   )
